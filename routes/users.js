2101b713f9428a8fb33374fe527efc5c95aed1e5
Aarchu-B <archanabaral2000@gmail.com>
2020-04-22 11:28:34 +0545

    Hash password

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
+const bcrypt= require('bcryptjs')
 const User = require("../models/User");
 
 //@route   POST api/users
@@ -15,14 +16,40 @@ router.post(
       "password",
       "Please enter password with 6 r more character"
     ).isLength({ min: 6 }),
-  ], 
-  (req, res) => {
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-         return res.status(400).json({errors:errors.array()});
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ msg: `User with this ${email} already exists` });
       }
-      res.send(req.body)
-    
+      user = new User({
+        name:name,
+        email,
+        password,
+      });
+      const salt = await bcrypt.genSalt(10);
+
+      user.password =await bcrypt.hash(password , salt);
+
+      await user.save()
+
+      res.send('user saved')
+
+      
+    } catch (err) {
+      console.log(err.message)
+      res.status(500).send('server error')
+    }
   }
 );
 module.exports = router;
